e58430d38d78af0d0dee2ce503edb70a6d9a3486
AdryGiuliani <giulianiadriano3@gmail.com>
2025-03-14 16:01:37 +0100

    rm libraries

--- src/database/initDb.js
+++ src/database/initDb.js
@@ -3,5 +3,8 @@
 import sqlite from "sqlite3";
 
 export function initDb() {
-    return new sqlite.Database("./surplusfile.sqlite");
+    return new sqlite.Database(
+        "./database/database.db",
+        sqlite.OPEN_READWRITE,
+    );
 }
--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
 "use strict";
 import {initDb} from "./database/initDb.js";
-
-initDb();
+let db = initDb();
+db.close()
