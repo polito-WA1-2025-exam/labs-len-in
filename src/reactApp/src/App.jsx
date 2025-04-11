6f7f61e0b0990ea66a4fe00ac220da263873e860
fede <fedef306@gmail.com>
2025-04-11 10:28:21 +0200

    updated App.jsx

--- src/reactApp/src/App.jsx
+++ src/reactApp/src/App.jsx
@@ -1,20 +1,18 @@
 import { useState } from 'react'
-import myicon from './assets/master.png'
-import react from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {Navheader} from "./components/Navheader.jsx";
 import {ViewStores} from "./components/ViewStores.jsx";
+import {ViewBags} from "./components/ViewBags.jsx";
 
 function App() {
-    const [count, setCount] = useState(0)
 
     return (
         <div className="flex-column" property={{width: parent, height: parent, backgroundColor: "red"}}>
             <Navheader/>
             <div className="card-body mt-3" property="width: max-content;">
                 <ViewStores items={['Store1', 'Store2', 'Store3']}/>
+                <ViewBags items={['Bag1', 'Bag2', 'Bag3']}/>
             </div>
         </div>
     )
--- src/reactApp/src/main.jsx
+++ src/reactApp/src/main.jsx
@@ -2,7 +2,6 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-import master_img from "./assets/img.png";
 
 // Extracted layout component that ensures a reactive layout
 const MainLayout = () => (
